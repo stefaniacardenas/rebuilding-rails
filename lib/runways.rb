d75d5ac859fd1240b2d7c6b5eff888b599a9f2fd
Stefania F. Cardenas <stefaniaferullo@hotmail.com>
2014-12-28 00:20:48 +0100

    Edited gem and created small app

--- lib/runways.rb
+++ lib/runways.rb
@@ -1,5 +1,10 @@
 require "runways/version"
 
 module Runways
-  # Your code goes here...
+  class Application
+    def call(env)
+      [200,{'Content-Type'=>'text/html'},
+        ["Hello from Ruby on Runways!"]]
+    end
+  end
 end
