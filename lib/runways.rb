36176fd1ce9e27a914c38fb5cbb8945a5c328515
Stefania Ferullo Cardenas <stefaniaferullocardenas@Stefanias-Mac-mini.local>
2015-01-13 13:39:07 +0000

    Created debug file

--- lib/runways.rb
+++ lib/runways.rb
@@ -3,7 +3,11 @@ require "runways/version"
 module Runways
   class Application
     def call(env)
+    	#This line should create a file called debug.txt when executed
+      `echo debug >debug.txt`
+			# This line returns success (200) and state that this will be an html content type
       [200,{'Content-Type'=>'text/html'},
+    	# This is the output of our call
         ["Hello from Ruby on Runways!"]]
     end
   end
